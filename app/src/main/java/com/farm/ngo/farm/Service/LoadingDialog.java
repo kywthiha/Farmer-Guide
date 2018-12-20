083492410da5caab2b1f637198a0739c07dbf4ac
PKUIT <thiha0905@gmail.com>
2018-12-20 23:49:09 +0630

    for my King

---
+++ app/src/main/java/com/farm/ngo/farm/Service/LoadingDialog.java
@@ -0,0 +1,4 @@
+package com.farm.ngo.farm.Service;
+
+public class LoadingDialog {
+}
---
+++ app/src/main/java/com/farm/ngo/farm/offlineInfoShow/CardCropItem.java
@@ -0,0 +1,11 @@
+package com.farm.ngo.farm.Holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class CardCropItem extends RecyclerView.ViewHolder{
+
+    public CardCropItem(View itemView) {
+        super(itemView);
+    }
+}
